2408a7acc20776556d8ef02e5c23cdb9f3a7d5de
sravan1433 <www.sravantudi2002@gmail.com>
2024-12-06 15:57:46 +0530

    changing with variables

--- variable1.sh
+++ variable1.sh
@@ -1,11 +1,10 @@
 #!bin/bash
 
-Person1=sunny
-Person2=mallesh
+Person1=sravan
+Person2=vittal
 
 
-echo "$Person1: Hi mallesh,good morning"
+echo "$Person1: Hi $Person2,good morning"
 echo "$Person2: goodmorning,how are you"
 echo "$Person1: goot,what about you"
 echo "$Person2: yaa good"
- 
